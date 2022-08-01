4ece7a726cf97e428df30d8ee9b3b553d41d8275
神秘布偶猫 <kannafay@qq.com>
2022-08-02 00:17:42 +0800

    2022.8.2

--- inc/head.php
+++ inc/head.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-transform" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+    <meta name="description" content="<?php bloginfo('description'); ?>" />
     <title><?php if(function_exists('show_wp_title')){show_wp_title();} ?></title>
     <link rel="shortcut icon" href="<?php site_icon_url(); ?>" type="image/x-icon" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/static/css/init.css">
