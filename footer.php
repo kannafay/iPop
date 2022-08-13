3239e979dea75b09f25b5b14cde879befd864726
神秘布偶猫 <kannafay@qq.com>
2022-08-14 02:17:26 +0800

    2022.8.14 2:17

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer container">
     <div class="footer-box">
-        <div class="copyright">© 2022 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name') ?></a> All copyright.</div>
+        <div class="copyright">© 2022 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> All copyright.</div>
     </div>
 </footer>
